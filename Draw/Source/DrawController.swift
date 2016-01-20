7b495c6199e4c228a607181b02f98174dc979367
Julian Grosshauser <julian@juliangrosshauser.com>
2016-01-20 23:33:50 +0100

    Add fullscreen image view as subview

--- Draw/Source/DrawController.swift
+++ Draw/Source/DrawController.swift
@@ -13,8 +13,15 @@ class DrawController: UIViewController {
     //MARK: Properties
 
     private var previousTouchLocation: CGPoint?
+    private let imageView = UIImageView()
 
     //MARK: UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.frame = view.frame
+        view.addSubview(imageView)
+    }
 
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         previousTouchLocation = touches.first?.locationInView(view)
